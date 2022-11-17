23247aaed8a389654868c20e4260cb26959fa86c
SevenCatsHere <sevencats@tripsit.me>
2022-11-17 15:16:59 -0500

    finish drug category tests

--- server/schema/drug/__tests__/category.spec.ts
+++ server/schema/drug/__tests__/category.spec.ts
@@ -169,3 +169,223 @@ describe('Query', () => {
     });
   });
 });
+
+describe('Mutation', () => {
+  let newDrugCategoryId: string;
+
+  test('createDrugCategory', async () => {
+    const { body } = await server.executeOperation({
+      query: gql`
+        mutation CreateDrugCategory($name: String!, $type: DrugCategoryType!) {
+          createDrugCategory(name: $name, type: $type) {
+            id
+            name
+            type
+            createdAt
+          }
+        }
+      `,
+      variables: {
+        name: 'Benzodiazapine',
+        type: 'CHEMICAL',
+      },
+    }, {
+      contextValue: await createTestContext(knex, discordApi),
+    });
+
+    assert(body.kind === 'single');
+    expect(body.singleResult.errors).toBeUndefined();
+    expect(body.singleResult.data).toEqual({
+      createDrugCategory: {
+        id: expect.stringMatching(uuidPattern),
+        name: 'Benzodiazapine',
+        type: 'CHEMICAL',
+        createdAt: expect.any(Date),
+      },
+    });
+
+    newDrugCategoryId = (body.singleResult.data!.createDrugCategory as { id: string }).id;
+  });
+
+  test('associateDrugWithCategory', async () => {
+    const { body } = await server.executeOperation({
+      query: gql`
+        mutation AssociateDrugWithCategory($drugId: UUID!, $drugCategoryId: UUID!) {
+          associateDrugWithCategory(drugId: $drugId, drugCategoryId: $drugCategoryId) {
+            id
+            name
+            categories {
+              id
+              name
+              type
+              createdAt
+            }
+          }
+        }
+      `,
+      variables: {
+        drugId: lsdId,
+        drugCategoryId: newDrugCategoryId,
+      },
+    }, {
+      contextValue: await createTestContext(knex, discordApi),
+    });
+
+    assert(body.kind === 'single');
+    expect(body.singleResult.errors).toBeUndefined();
+    expect(body.singleResult.data).toEqual({
+      associateDrugWithCategory: {
+        id: lsdId,
+        name: 'LSD',
+        categories: [{
+          id: newDrugCategoryId,
+          name: 'Benzodiazapine',
+          type: 'CHEMICAL',
+          createdAt: expect.any(Date),
+        }],
+      },
+    });
+  });
+
+  test('disassociateDrugFromCategory', async () => {
+    const { body } = await server.executeOperation({
+      query: gql`
+        mutation disassociateDrugFromCategory($drugId: UUID!, $drugCategoryId: UUID!) {
+          disassociateDrugFromCategory(drugId: $drugId, drugCategoryId: $drugCategoryId) {
+            id
+            name
+            categories {
+              id
+              name
+              type
+              createdAt
+            }
+          }
+        }
+      `,
+      variables: {
+        drugId: lsdId,
+        drugCategoryId: newDrugCategoryId,
+      },
+    }, {
+      contextValue: await createTestContext(knex, discordApi),
+    });
+
+    assert(body.kind === 'single');
+    expect(body.singleResult.errors).toBeUndefined();
+    expect(body.singleResult.data).toEqual({
+      disassociateDrugFromCategory: {
+        id: lsdId,
+        name: 'LSD',
+        categories: [],
+      },
+    });
+  });
+
+  test('deleteDrugCategory', async () => {
+    await expect(knex('drugCategories')
+      .where('id', newDrugCategoryId)
+      .first()
+      .then(Boolean))
+      .resolves.toBe(true);
+
+    const { body } = await server.executeOperation({
+      query: gql`
+        mutation DeleteDrugCategory($id: UUID!) {
+          deleteDrugCategory(id: $id)
+        }
+      `,
+      variables: {
+        id: newDrugCategoryId,
+      },
+    }, {
+      contextValue: await createTestContext(knex, discordApi),
+    });
+
+    assert(body.kind === 'single');
+    expect(body.singleResult.errors).toBeUndefined();
+    expect(body.singleResult.data).toEqual({
+      deleteDrugCategory: null,
+    });
+
+    await expect(knex('drugCategories')
+      .where('id', newDrugCategoryId)
+      .first()
+      .then(Boolean))
+      .resolves.toBe(false);
+  });
+});
+
+describe('DrugCategory', () => {
+  let categoryId: string;
+  beforeAll(async () => {
+    categoryId = await server.executeOperation({
+      query: gql`
+        mutation CreateDrugCategory($name: String!, $type: DrugCategoryType!) {
+          createDrugCategory(name: $name, type: $type) {
+            id
+            name
+            type
+            createdAt
+          }
+        }
+      `,
+      variables: {
+        name: 'Benzodiazapine',
+        type: 'CHEMICAL',
+      },
+    }, {
+      contextValue: await createTestContext(knex, discordApi),
+    })
+      .then(({ body }) => {
+        if (body.kind !== 'single') throw new Error('Invalid response for creating category');
+        return (body.singleResult.data!.createDrugCategory as { id: string }).id;
+      });
+
+    await server.executeOperation({
+      query: gql`
+        mutation associateDrugWithCategory($drugId: UUID!, $drugCategoryId: UUID!) {
+          associateDrugWithCategory(drugId: $drugId, drugCategoryId: $drugCategoryId) {
+            id
+          }
+        }
+      `,
+      variables: {
+        drugId: lsdId,
+        drugCategoryId: categoryId,
+      },
+    }, {
+      contextValue: await createTestContext(knex, discordApi),
+    });
+  });
+
+  test('drugs', async () => {
+    const { body } = await server.executeOperation({
+      query: gql`
+        query CategoryDrugs($id: UUID!) {
+          drugCategories(id: $id) {
+            drugs {
+              id
+              name
+            }
+          }
+        }
+      `,
+      variables: {
+        id: categoryId,
+      },
+    }, {
+      contextValue: await createTestContext(knex, discordApi),
+    });
+
+    assert(body.kind === 'single');
+    expect(body.singleResult.data).toEqual({
+      drugCategories: [{
+        drugs: [{
+          id: lsdId,
+          name: 'LSD',
+        }],
+      }],
+    });
+  });
+});
